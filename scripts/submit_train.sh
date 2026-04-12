f7cf1d584ad98aed123c951d69cab430bcd5a2ab
vardy <mythvardy@gmail.com>
2026-04-12 13:19:04 -0400

    update SLURM scripts for H100 partition and fix module/PYTHONPATH

--- scripts/submit_train.sh
+++ scripts/submit_train.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH -c 12
-#SBATCH -t 2:30:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:a100:1
+#SBATCH -c 8
+#SBATCH --gres=gpu:h100:1
+#SBATCH -t 24:00:00
+#SBATCH --partition=gpu-h100
 #SBATCH --account=hpcintro-aac
 #SBATCH --output=experiments/slurm-%j.out
 
 echo "Running $SLURM_NTASKS tasks ($SLURM_CPUS_PER_TASK cores) on $SLURM_NODELIST"
 
 module purge
-module load cuda/12.3.0/gcc/11.3.0/zen2
-module load python/3.10.10/gcc/11.3.0/cuda/12.3.0/linux-rhel8-zen2
+module load python/3.10.10/gcc/11.3.0/cuda/12.3.0/linux-rhel8-x86_64
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export PYTHONPATH=$HOME/.local/lib/python3.10/site-packages:$PYTHONPATH
